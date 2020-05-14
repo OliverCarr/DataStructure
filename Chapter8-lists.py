6db0e4fb43fb5cf89cc15f20f6f8a762e922e14a
Oliver Carr <olivercarrorg@gmail.com>
2020-05-14 12:30:32 +0100

    Chapter 8

---
+++ Chapter8-lists.py
@@ -0,0 +1,30 @@
+
+#A list is a sequence
+
+#A list is a sequence of values. 
+#These values of any type are 'elements' or 'items'
+
+#Some simple lists
+[10, 20 ,30, 40]
+['one', 'two', 'three']
+
+#More complex, mixed lists
+['spam', 2.0, 5, [10, 20]]
+
+#A list within a list is nested
+
+#an empty list can be creating with squre brackets
+[]
+
+cheese = ['Cheddar', 'Edam', 'Gouda']
+numbers = [17, 123]
+empty = []
+#print(cheese, numbers, empty)
+
+#Lists are mutable 
+
+print(cheese[0])
+
+numbers = [17, 123]
+numbers[1] = 5
+print(numbers)
