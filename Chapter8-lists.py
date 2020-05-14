f1a12f65cd8bd0a05753eafee1bc608702303517
Oliver Carr <olivercarrorg@gmail.com>
2020-05-14 14:52:09 +0100

    8.7

--- Chapter8-lists.py
+++ Chapter8-lists.py
@@ -11,7 +11,9 @@
 #More complex, mixed lists
 ['spam', 2.0, 5, [10, 20]]
 
-#A list within a list is nested
+#A list within a list is nested,
+#the nested list still counts as a songle element
+#this list is length 4
 
 #an empty list can be creating with squre brackets
 []
@@ -22,7 +24,7 @@ empty = []
 #print(cheese, numbers, empty)
 
 #Lists are mutable 
-
+'''
 print(cheese[0])
 
 numbers = [17, 123]
@@ -42,4 +44,80 @@ print(numbers)
 
 print('Chalk' in cheese)
 #The in operator also works
+'''
+#Traversing a list
+'''
+for cheese in cheese:   #The for loop is weird
+    print(cheese)   
 
+
+numbers = [1,2,3,4,5,6,7,8,9]
+print(numbers)
+
+for i in range(len(numbers)):
+    numbers[i] = numbers[i] * 2
+print(numbers)
+
+for x in empty:
+    print('This never happens.')
+#there are no elements in empty so body is never executed
+'''
+#List operations
+'''
+a= [1,2,3]
+b= [4,5,6]
+c = a + b
+print(c)
+
+d = a*3
+print(d)
+#Addition and multiplication concatinate lists 
+'''
+#List slices
+'''
+t = ['a', 'b', 'c', 'd', 'e', 'f'] 
+print(t[1:3])
+
+print(t[:4])
+
+print(t[3:])
+
+t[1:3] = ['x', 'y']
+print(t)
+'''
+#List methods
+'''
+t= ['a', 'b', 'c']
+t.append('d')   #Stick an element on the end
+print(t)
+
+t1 = ['a', 'b', 'c']
+t2 = ['d', 'e']
+t1.extend(t2)   #Stick a list on the end
+print(t1)
+
+t = ['d','a','c','b','e']
+t.sort()    #puts it in order
+print(t)
+'''
+#Deleting elements
+
+t = ['a','b','c']
+x = t.pop(1)    #'Pops out' one element and stores it
+print(t)
+print(x)
+
+
+t = ['a','b','c']
+del t[1]   #deletes one element without storeing it
+print(t)
+
+t = ['a','b','c']
+t.remove('b')   #searthes the list and removes its argument
+print(t)
+
+
+t = ['a','b','c']
+t.extend(['d', 'e', 'f'])
+del t[1:5]      #upto, but not including, the second element
+print(t)
